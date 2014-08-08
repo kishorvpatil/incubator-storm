a0905a108a0d8c2a1b28e4ad4ebbb6bbf16db49b
Kishor Patil <kpatil@yahoo-inc.com>
2014-08-08 13:49:38 -0500

    Adding comments and asserts

--- storm-core/test/jvm/backtype/storm/utils/StormBoundedExponentialBackoffRetryTest.java
+++ storm-core/test/jvm/backtype/storm/utils/StormBoundedExponentialBackoffRetryTest.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package backtype.storm.utils;
 
 import junit.framework.TestCase;
@@ -45,16 +62,32 @@ public class StormBoundedExponentialBackoffRetryTest extends TestCase {
         StormBoundedExponentialBackoffRetry retryPolicy = new StormBoundedExponentialBackoffRetry(baseSleepMs, maxSleepMs, maxRetries);
         int retryCount = 0;
         int prevSleepMs = 0;
-        LOG.info("The baseSleepMs [" + baseSleepMs + "] the maxSleepMs [" + maxSleepMs + "] the maxRetries [" + maxRetries + "]");
+        LOG.info("The baseSleepMs [" + baseSleepMs + "] the maxSleepMs [" + maxSleepMs +
+                "] the maxRetries [" + maxRetries + "]");
         while (retryCount <= maxRetries) {
             int currSleepMs = retryPolicy.getSleepTimeMs(retryCount, 0);
-            LOG.info("For retryCount [" + retryCount + "] the previousSleepMs [" + prevSleepMs + "] the currentSleepMs [" + currSleepMs + "]");
-            assertTrue("For retryCount [" + retryCount + "] the previousSleepMs [" + prevSleepMs + "] is not less than currentSleepMs [" + currSleepMs + "]", (prevSleepMs < currSleepMs) || (currSleepMs == maxSleepMs));
-            assertTrue("For retryCount [" + retryCount + "] the currentSleepMs [" + currSleepMs + "] is less than baseSleepMs [" + baseSleepMs + "].", (baseSleepMs <= currSleepMs) || (currSleepMs == maxSleepMs));
-            assertTrue("For retryCount [" + retryCount + "] the currentSleepMs [" + currSleepMs + "] is greater than maxSleepMs [" + maxSleepMs + "]", maxSleepMs >= currSleepMs);
+            LOG.info("For retryCount [" + retryCount + "] the previousSleepMs [" + prevSleepMs +
+                    "] the currentSleepMs [" + currSleepMs + "]");
+            assertTrue("For retryCount [" + retryCount + "] the previousSleepMs [" + prevSleepMs +
+                            "] is not less than currentSleepMs [" + currSleepMs + "]",
+                    (prevSleepMs < currSleepMs) || (currSleepMs == maxSleepMs));
+            assertTrue("For retryCount [" + retryCount + "] the currentSleepMs [" + currSleepMs +
+                            "] is less than baseSleepMs [" + baseSleepMs + "].",
+                    (baseSleepMs <= currSleepMs) || (currSleepMs == maxSleepMs));
+            assertTrue("For retryCount [" + retryCount + "] the currentSleepMs [" + currSleepMs +
+                            "] is greater than maxSleepMs [" + maxSleepMs + "]",
+                    maxSleepMs >= currSleepMs);
             prevSleepMs = currSleepMs;
             retryCount++;
         }
+        int badRetryCount = maxRetries + 10;
+        int currSleepMs = retryPolicy.getSleepTimeMs(badRetryCount, 0);
+        LOG.info("For badRetryCount [" + badRetryCount + "] the previousSleepMs [" + prevSleepMs +
+                "] the currentSleepMs [" + currSleepMs + "]");
+        assertTrue("For the badRetryCount [" + badRetryCount + "] that's greater than maxRetries [" +
+                        maxRetries + "]the currentSleepMs [" + currSleepMs + "] " +
+                        "is greater than maxSleepMs [" + maxSleepMs + "]",
+                maxSleepMs >= currSleepMs);
     }
 }
 
